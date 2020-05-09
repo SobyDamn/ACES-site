f6b88cae12894ea72e3b5538d7eb2933c7f9f00f
SobyDamn <sobydanny@gmail.com>
2020-05-09 14:09:11 +0530

    added firebase

---
+++ scripts/firebase-auth.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCDwxyVvKg8gTjTxyS3DS27J-Vfi93bwLw",
+    authDomain: "aces-cs.firebaseapp.com",
+    databaseURL: "https://aces-cs.firebaseio.com",
+    projectId: "aces-cs",
+    storageBucket: "aces-cs.appspot.com",
+    messagingSenderId: "593702771317",
+    appId: "1:593702771317:web:8530596233072fd0a6363c",
+    measurementId: "G-CMX0SQZ70V"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
